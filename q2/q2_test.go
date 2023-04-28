3658c5b660cccdb605bd61c9fc1de370bfcea370
miltonpedro <126252785+miltonpedro@users.noreply.github.com>
2023-04-28 09:57:33 -0300

    Update q2_test.go

--- q2/q2_test.go
+++ q2/q2_test.go
@@ -4,113 +4,18 @@ import (
 	"fmt"
 	"testing"
 )
-
-func TestProblemsSolved(t *testing.T) {
-	tests := []struct {
-		answers [][3]bool
-		want    int
-	}{
-		{
-			answers: [][3]bool{
-				{true, true, true},
-				{true, true, false},
-				{true, false, false},
-			},
-			want: 2,
-		},
-		{
-			answers: [][3]bool{
-				{false, true, false},
-				{false, true, true},
-				{false, false, false},
-			},
-			want: 1,
-		},
-		{
-			answers: [][3]bool{
-				{false, true, false},
-				{false, true, true},
-				{false, false, false},
-				{true, true, true},
-				{true, true, false},
-			},
-			want: 3,
-		},
-		{
-			answers: [][3]bool{
-				{false, true, false},
-			},
-			want: 0,
-		},
-		{
-			answers: [][3]bool{
-				{true, false, false},
-				{true, true, true},
-			},
-			want: 1,
-		},
-		{
-			answers: [][3]bool{
-				{false, false, false},
-				{false, true, true},
-				{true, true, true},
-				{false, true, false},
-				{true, false, true},
-				{true, true, true},
-				{false, false, true},
-				{true, false, false},
-				{true, true, false},
-				{true, false, true},
-				{false, true, false},
-				{false, false, true},
-				{true, true, false},
-				{false, true, false},
-				{true, true, false},
-				{false, false, false},
-				{true, true, true},
-				{true, false, true},
-				{false, false, true},
-				{true, true, false},
-				{true, true, true},
-				{false, true, true},
-				{true, true, false},
-				{false, false, false},
-				{false, false, false},
-				{true, true, true},
-				{false, false, false},
-				{true, true, true},
-				{false, true, true},
-				{false, false, true},
-				{false, false, false},
-				{false, false, false},
-				{true, true, false},
-				{true, true, false},
-				{true, false, true},
-				{true, false, false},
-				{true, false, true},
-				{true, false, true},
-				{false, true, true},
-				{true, true, false},
-				{true, true, false},
-				{false, true, false},
-				{true, false, true},
-				{false, false, false},
-				{false, false, false},
-				{false, false, false},
-				{false, false, true},
-				{true, true, true},
-				{false, true, true},
-				{true, false, true},
-			},
-			want: 29,
-		},
-	}
-
-	for i, test := range tests {
-		t.Run(fmt.Sprintf("test %d", i+1), func(t *testing.T) {
-			if got := ProblemsSolved(test.answers); got != test.want {
-				t.Errorf("ProblemsSolved() = %v, want %v", got, test.want)
+func ProblemsSolved(answers [][3]bool) int {
+	contagem := 0
+	for i := 0; i < len(answers); i++ {
+		certezas := 0
+		for j := 0; j < 3; j++ {
+			if answers[i][j] {
+				certezas++
 			}
-		})
+		}
+		if certezas >= 2 {
+			contagem++
+		}
 	}
+	return contagem
 }
