d82bd48caa0d089274d99f5c08a587decb93f673
miltonpedro <126252785+miltonpedro@users.noreply.github.com>
2023-04-28 08:58:50 -0300

    Update q1_test.go

--- q1/q1_test.go
+++ q1/q1_test.go
@@ -1,14 +1,11 @@
-package main
-
-import "errors", "fmt"
-
 func DivideWatermelon(weight int) (bool, error) {
 	if weight <= 0 {
 		return false, nil errors.New("o peso da melancia deve ser maior que 0")
 	}
-	else weight %= 2 {
+	else weight %2 == 0 {
 		return true, nil
 	}
 	return false, nil
 
+}
 }
