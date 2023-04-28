4303cb467f7d6eee942432df67fc32821cec7cf5
miltonpedro <126252785+miltonpedro@users.noreply.github.com>
2023-04-28 10:44:54 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -19,7 +19,7 @@ func DivideWatermelon(weight int) (bool, error) {
 	if weight <= 0 {
 		return false, nil errors.New("o peso da melancia deve ser maior que 0")
 	}
-	else if weight % 2 == 0 {
+	if weight % 2 == 0 {
 		return true, nil
 	}
 	return false, nil
