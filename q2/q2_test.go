8821f45ae512e8a47004e15e8907a76f0e527a37
miltonpedro <126252785+miltonpedro@users.noreply.github.com>
2023-04-28 10:38:30 -0300

    Update q2_test.go

--- q2/q2_test.go
+++ q2/q2_test.go
@@ -1,18 +1,116 @@
 package q2
 
-//Um dia, três melhores amigos - Pedro, Vanessa e Tônia - decidiram formar uma equipe e participar de concursos de
-//programação. Os participantes geralmente recebem vários problemas durante esses concursos. Muito antes do início, os
-//amigos decidiram que implementariam um problema somente se pelo menos dois deles tivessem certeza da solução. Caso
-//contrário, os amigos não escreveriam a solução do problema.
-//
-//Este concurso oferece n problemas aos participantes. Para cada problema, sabemos qual amigo tem certeza da solução. Você
-//receberá uma matriz booleana de n linhas e 3 colunas, em que a i-ésima linha representa as opiniões de Pedro, Vanessa e
-//Tônia, respectivamente, sobre o i-ésimo problema. O valor "true" indica que o amigo tem certeza da solução, e "false"
-//indica o contrário.
-//
-//Ajude os amigos a encontrar o número de problemas para os quais eles escreverão uma solução.
+import (
+	"fmt"
+	"testing"
+)
 
-func ProblemsSolved(answers [][3]bool) int {
-	// Seu código aqui
-	return 0
+func TestProblemsSolved(t *testing.T) {
+	tests := []struct {
+		answers [][3]bool
+		want    int
+	}{
+		{
+			answers: [][3]bool{
+				{true, true, true},
+				{true, true, false},
+				{true, false, false},
+			},
+			want: 2,
+		},
+		{
+			answers: [][3]bool{
+				{false, true, false},
+				{false, true, true},
+				{false, false, false},
+			},
+			want: 1,
+		},
+		{
+			answers: [][3]bool{
+				{false, true, false},
+				{false, true, true},
+				{false, false, false},
+				{true, true, true},
+				{true, true, false},
+			},
+			want: 3,
+		},
+		{
+			answers: [][3]bool{
+				{false, true, false},
+			},
+			want: 0,
+		},
+		{
+			answers: [][3]bool{
+				{true, false, false},
+				{true, true, true},
+			},
+			want: 1,
+		},
+		{
+			answers: [][3]bool{
+				{false, false, false},
+				{false, true, true},
+				{true, true, true},
+				{false, true, false},
+				{true, false, true},
+				{true, true, true},
+				{false, false, true},
+				{true, false, false},
+				{true, true, false},
+				{true, false, true},
+				{false, true, false},
+				{false, false, true},
+				{true, true, false},
+				{false, true, false},
+				{true, true, false},
+				{false, false, false},
+				{true, true, true},
+				{true, false, true},
+				{false, false, true},
+				{true, true, false},
+				{true, true, true},
+				{false, true, true},
+				{true, true, false},
+				{false, false, false},
+				{false, false, false},
+				{true, true, true},
+				{false, false, false},
+				{true, true, true},
+				{false, true, true},
+				{false, false, true},
+				{false, false, false},
+				{false, false, false},
+				{true, true, false},
+				{true, true, false},
+				{true, false, true},
+				{true, false, false},
+				{true, false, true},
+				{true, false, true},
+				{false, true, true},
+				{true, true, false},
+				{true, true, false},
+				{false, true, false},
+				{true, false, true},
+				{false, false, false},
+				{false, false, false},
+				{false, false, false},
+				{false, false, true},
+				{true, true, true},
+				{false, true, true},
+				{true, false, true},
+			},
+			want: 29,
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test %d", i+1), func(t *testing.T) {
+			if got := ProblemsSolved(test.answers); got != test.want {
+				t.Errorf("ProblemsSolved() = %v, want %v", got, test.want)
+			}
+		})
+	}
 }
